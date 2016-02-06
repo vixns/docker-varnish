9eab4c3ec9571aecb398075bb8d35cc9db46cbc5
Stéphane Cottin <stephane.cottin@vixns.com>
2016-02-06 10:21:39 +0100

    fix for reload-vcl

--- start.sh
+++ start.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+cat > /etc/default/varnish << EOF
+DAEMON_OPTS="-a :$LISTEN_PORT -T :$TELNET_PORT -f /etc/varnish/default.vcl -S /etc/varnish/secret -s $STORAGE_BACKEND,$CACHE_SIZE"
+EOF
+
 exec varnishd -F \
   -f $VCL_CONFIG \
-  -T 0.0.0.0:$TELNET_PORT \
-  -a 0.0.0.0:$LISTEN_PORT \
+   -S /etc/varnish/secret \
+  -T :$TELNET_PORT \
+  -a :$LISTEN_PORT \
   -s $STORAGE_BACKEND,$CACHE_SIZE \
   $VARNISHD_PARAMS
 
